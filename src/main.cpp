bb9e16b32ffaba1639dc28a9ff7e3a4571316a0d
TheMaybeast <59030276+TheMaybeast@users.noreply.github.com>
2024-01-09 15:07:38 -0300

    Adds ability to unload with SHV reset

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "Utils/logger.hpp"
 #include "Utils/ExtraRepairPatch.hpp"
 #include "Utils/version.h"
+#include "main.h"
 #include <windows.h>
+
+void Main(){
+    while(true){
+        WAIT(0);
+    }
+}
 
 BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved) {
     Log::SetFile(".\\ExtraRepairPatch.log");
@@ -9,10 +16,12 @@ BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved) {
         case DLL_PROCESS_ATTACH:
             Log::Write(INFO, std::format("====== Loading ExtraRepairPatch v{}.{}.{} ======", VERSION));
             ExtraRepairPatch::Patch();
+            scriptRegister(hInstance, Main);
             break;
         case DLL_PROCESS_DETACH:
-            Log::Write(INFO, "====== Unloading ExtraRepairPatch ======");
             ExtraRepairPatch::Remove();
+            Log::Write(INFO, std::format("====== Unloaded ExtraRepairPatch v{}.{}.{} ======", VERSION));
+            scriptUnregister(hInstance);
             break;
         default:
             break;
